ed88f08124358334b3a3a086785b8123e4bbaf2b
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-25 21:31:06 +0200

    not found page added

--- frontend/src/components/EmployeeCard.vue
+++ frontend/src/components/EmployeeCard.vue
@@ -1,16 +1,22 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="mx-auto flex w-4/5 flex-col items-center rounded p-2 sm:w-fit">
+  <div class="max-w-64 mx-auto flex flex-col items-center rounded p-2 sm:w-fit">
     <!-- Added "items-center" class -->
     <img
       alt="employees 1"
-      class="rounded-xl"
+      class="w-full rounded-xl"
       src="@/assets/images/employees/1.jpg"
     />
-    <h3>Михаил Шепеленко</h3>
-    <div class="">менеджер</div>
-    <div class="">+7 (999) 999-99-99</div>
+    <div class="mt-1 flex flex-col items-center gap-1">
+      <div class="flex justify-around">
+        <h3 class="text-lg">Михаил Шепеленко</h3>
+        <div class="">Менеджер</div>
+      </div>
+
+      <div class=""></div>
+      <div class=""><a href="">+7 (999) 999-99-99</a></div>
+    </div>
   </div>
 </template>
 
--- frontend/src/views/PageNotFound.vue
+++ frontend/src/views/PageNotFound.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-
+  <main class="flex flex-col items-center justify-center py-24">
+    <div class="flex flex-col items-center px-24">
+      <div class="font-sans text-xl font-medium text-red-500">
+        <img alt="page not found" class="mb-4" src="@/assets/images/404.svg" />
+        Страница не найдена
+      </div>
+    </div>
+    <router-link exact to="/">
+      <button class="mt-5 rounded-xl bg-neutral-400 px-3.5 py-2 text-white">
+        Вернуться на главную страницу
+      </button>
+    </router-link>
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
