f620f809734f250e7483f9b5d7c0667141b73a5d
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-26 02:40:32 +0200

    Smooth scroll added

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,12 @@
 <script setup>
 import JobCard from "@/components/JobCard.vue";
+import { scrollToSection } from "@/components/scrollToElement";
 </script>
 
 <template>
-  <main class="font-sans">
+  <main class="scroll-smooth font-sans">
     <div class="container">
-      <div class="flex flex-wrap items-center justify-around">
+      <section id="top" class="flex flex-wrap items-center justify-around">
         <div class="">
           <div class="mb-4 md:mb-10">
             <h1 class="text-title font-bold">
@@ -16,21 +17,51 @@ import JobCard from "@/components/JobCard.vue";
 
           <button
             class="btn rounded-xl bg-red-500 px-3.5 py-2 text-primary-white"
+            @click="scrollToSection('jobs')"
           >
             Смотреть вакансии
           </button>
         </div>
         <img alt="" class="w-max" src="@/assets/images/home-image.jpg" />
-      </div>
+      </section>
 
-      <section class="text-center">
+      <section id="jobs" class="text-center">
         <h2 class="text-xl">Вакансии:</h2>
         <div>
           <JobCard :job="{ name: 'kifli' }"></JobCard>
         </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
+        <div class="mb-4 h-40 bg-neutral-200">
+          dgasdgdfsgfds df sg sdf g sdfg d sfg sdf g sdfg sdf g dfs g s fg sd g
+          sdg sd g fsd g dfs g
+        </div>
       </section>
+      <a>scroll</a>
     </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+#top {
+  min-height: calc(100vh - 100px);
+}
+</style>
