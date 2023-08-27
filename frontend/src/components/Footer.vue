1d1844e3e373f27f2bc6bcd265d0cdb4fcaa47e4
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-28 01:11:42 +0200

    footer fixed

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 
 <template>
-  <footer class="rounded-lg border-t-2 bg-white">
+  <footer class="-mt-3 rounded-t-2xl border-t-2 bg-white">
     <div class="mx-auto w-full max-w-screen-xl p-4 md:py-6">
       <div
         class="flex flex-col-reverse items-center gap-3 sm:items-center sm:justify-between md:flex-row"
       >
         <span
-          class="block text-sm text-gray-500 dark:text-gray-400 sm:text-center"
+          class="block text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-center"
           >© 2023 <span class="">ElitaWork</span>. All Rights Reserved.</span
         >
         <div class="flex items-center gap-2">
@@ -15,7 +15,7 @@
             class="flex items-center gap-2 text-gray-600 transition-all duration-300 hover:underline dark:text-gray-400"
             href="https://www.facebook.com/people/Elita-Work/pfbid06N8b2ygJWHi3EKDzxDU7s5eufqnQwhDe84QsCWAxMYPdaWcbgYh5cbtJBW5zwNFjl/?locale=hu_HU"
           >
-            <span>Мы в Facebook:</span>
+            <span class="font-jost">Мы в Facebook:</span>
             <img
               alt="facebook"
               class="h-6 w-6"
